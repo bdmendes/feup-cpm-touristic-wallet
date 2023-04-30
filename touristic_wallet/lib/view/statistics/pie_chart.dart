8318b8e5ed4397b1107f2f15b4c6e28165cf2b42
fernandorego <fernandoluisrego@gmail.com>
2023-04-30 17:44:29 +0100

    Finish pie chart

--- touristic_wallet/lib/view/statistics/pie_chart.dart
+++ touristic_wallet/lib/view/statistics/pie_chart.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../provider/amounts_provider.dart';
+import '../../provider/exchange_rates_provider.dart';
 import 'indicator.dart';
 
 class StatisticsPieChart extends StatefulWidget {
@@ -12,134 +17,101 @@ class StatisticsPieChart extends StatefulWidget {
 
 class StatisticsPieChartState extends State {
   int touchedIndex = -1;
+  var colors = <String, int>{};
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: <Widget>[
-          Expanded(
-            child: PieChart(
-              PieChartData(
-                pieTouchData: PieTouchData(
-                  touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                    setState(() {
-                      if (!event.isInterestedForInteractions ||
-                          pieTouchResponse == null ||
-                          pieTouchResponse.touchedSection == null) {
-                        touchedIndex = -1;
-                        return;
-                      }
-                      touchedIndex = pieTouchResponse
-                          .touchedSection!.touchedSectionIndex;
-                    });
-                  },
-                ),
-                borderData: FlBorderData(
-                  show: false,
-                ),
-                sectionsSpace: 5,
-                centerSpaceRadius: 50,
-                sections: showingSections(),
-              ),
-            ),
-          ),
-          const Wrap(
-            spacing: 12.0, // gap between adjacent chips
-            runSpacing: 8.0, // gap between lines
-            direction: Axis.horizontal,
-            children: <Widget>[
-              Indicator(
-                color: Color(0xFF2196F3),
-                text: 'First',
-                isSquare: false,
-              ),
-              Indicator(
-                color: Color(0xFFFFC300),
-                text: 'Second',
-                isSquare: false,
-              ),
-              Indicator(
-                color: Color(0xFF6E1BFF),
-                text: 'Third',
-                isSquare: false,
-              ),
-              Indicator(
-                color: Color(0xFF3BFF49),
-                text: 'Fourth',
-                isSquare: false,
-              )
-            ],
-          ),
-          const SizedBox(
-            width: 28,
-          ),
-        ],
-      );
-    //);
+    return Consumer2<AmountsProvider, ExchangeRatesProvider>(
+      builder: (context, amountsProvider, exchangeRatesProvider, child) {
+        return FutureBuilder(
+          future: amountsProvider.getExchangeAmounts(exchangeRatesProvider),
+          builder:  (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: <Widget>[
+                  Expanded(
+                    child: PieChart(
+                      PieChartData(
+                        pieTouchData: PieTouchData(
+                          touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                            setState(() {
+                              if (!event.isInterestedForInteractions ||
+                                  pieTouchResponse == null ||
+                                  pieTouchResponse.touchedSection == null) {
+                                touchedIndex = -1;
+                                return;
+                              }
+                              touchedIndex = pieTouchResponse
+                                  .touchedSection!.touchedSectionIndex;
+                            });
+                          },
+                        ),
+                        borderData: FlBorderData(
+                          show: false,
+                        ),
+                        sectionsSpace: 0,
+                        centerSpaceRadius: 50,
+                        sections: showingSections(snapshot.data!),
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                      spacing: 12.0, // gap between adjacent chips
+                      runSpacing: 8.0, // gap between lines
+                      direction: Axis.horizontal,
+                      children: showingIndicators(amountsProvider,exchangeRatesProvider)
+                  ),
+                  const SizedBox(
+                    width: 28,
+                  ),
+                ],
+              );
+            } else {
+              return const Text(
+                'Total: Unknown123',
+                style: TextStyle(fontSize: 20),
+              );
+            }
+          }
+        );
+      }
+    );
   }
 
-  List<PieChartSectionData> showingSections() {
-    return List.generate(4, (i) {
+  List<PieChartSectionData> showingSections(Map<String, double> exchangeAmounts) {
+    return List.generate(exchangeAmounts.length, (i) {
       final isTouched = i == touchedIndex;
       final fontSize = isTouched ? 30.0 : 20.0;
       final radius = isTouched ? 130.0 : 100.0;
       const shadows = [Shadow(color: Colors.black, blurRadius: 2)];
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            color: const Color(0xFF2196F3),
-            value: 40,
-            title: '40%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 1:
-          return PieChartSectionData(
-            color: const Color(0xFFFFC300),
-            value: 30,
-            title: '30%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 2:
-          return PieChartSectionData(
-            color: const Color(0xFF6E1BFF),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        case 3:
-          return PieChartSectionData(
-            color: const Color(0xFF3BFF49),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-              fontSize: fontSize,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: shadows,
-            ),
-          );
-        default:
-          throw Error();
+      var key = exchangeAmounts.keys.toList()[i];
+      var total = exchangeAmounts.values.reduce((a, b) => a + b);
+      if (!colors.containsKey(key)) {
+        colors[key] = Random().nextInt(0xffffffff);
       }
+      return PieChartSectionData(
+        color: Color(colors[key]!).withAlpha(0xff),
+        value: exchangeAmounts[key],
+        title: exchangeAmounts[key]! / total < 0.03 ? "" : exchangeAmounts[key]!.toStringAsFixed(2),
+        radius: radius,
+        titleStyle: TextStyle(
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          shadows: shadows,
+        ),
+      );
+    });
+  }
+
+  List<Indicator> showingIndicators(
+      AmountsProvider amountsProvider, ExchangeRatesProvider exchangeRatesProvider) {
+    return List.generate(amountsProvider.amounts.length, (i) {
+      return Indicator(
+        color: Color(colors[amountsProvider.amounts[i].currency]!).withAlpha(0xff),
+        text: amountsProvider.amounts[i].currency,
+        isSquare: false,
+      );
     });
   }
 }
