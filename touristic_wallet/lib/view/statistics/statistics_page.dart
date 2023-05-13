ec53f5da48426c041c897d3d47f74f90d6a5e75d
fernandorego <fernandoluisrego@gmail.com>
2023-05-13 16:16:49 +0100

    Minor fix

--- touristic_wallet/lib/view/statistics/statistics_page.dart
+++ touristic_wallet/lib/view/statistics/statistics_page.dart
@@ -11,7 +11,7 @@ class StatisticsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
           bottom: TabBar(
